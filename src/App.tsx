27720562daa67b68d4333987b658d260a37e33a1
Chaeyeon An <dkscodus1204@ewhain.net>
2024-05-13 23:30:03 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import accLogo from "../src/assets/acc_logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={accLogo} className="App-logo" alt="logo" />
+        <h1>⛅ ACC EWHA 2nd 최고짱짱맨 ⛅</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
